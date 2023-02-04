c8f938dde298ae666c51a2097707b4076a406dfb
dr0pp3dpack3ts <ali18@wgu.edu>
2023-02-03 18:28:16 -0800

    modified:   ignition.sh

--- ignition.sh
+++ ignition.sh
@@ -1,6 +1,5 @@
 #Install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sed 's:env zsh::g' | sed 's:chsh -s .*$::g')"
 
 #Install homebrew
 sudo mkdir homebrew && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C homebrew
@@ -37,13 +36,13 @@ git clone https://github.com/dr0pp3dpack3ts/openssh-files.git
 cd openssh-files
 ./openvpn.sh --import-config --config-file=cyberalvin.ovpn
 
-#Install powerlevel10k
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+# #Install powerlevel10k
+# git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
-zsh_theme="powerlevel10k/powerlevel10k"
-sed -i '' "s/robbyrussell/$zsh_theme/g" ~/.zshrc
+# zsh_theme="powerlevel10k/powerlevel10k"
+# sed -i '' "s/robbyrussell/$zsh_theme/g" ~/.zshrc
 
-/usr/bin/script ~/Desktop/iterm2.log
+
 
 
 #Starting MACOS services
